d91992691742ad373e8def513211a778329fddce
Nokias34 <63350109+Nokias34@users.noreply.github.com>
2020-06-25 11:12:01 +0700

    change configuration db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "us-cdbr-east-05.cleardb.net",
-    USER: "b9ad173eb97efd",
-    PASSWORD: "2529ec80",
-    DB: "heroku_b85dba1e77c7a7b",
+    HOST: "ftpupload.net",
+    USER: "epiz_26099114",
+    PASSWORD: "hDN7N2VXikQn",
+    DB: "epiz_26099114_articles",
     dialect: "mysql",
     pool: {
       max: 105,
